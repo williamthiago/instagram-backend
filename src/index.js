392333e0219a3df5fc997bc69fffb9b0ec7c6a5d
William Thiago <williamthiago@gmail.com>
2019-06-12 01:20:41 -0400

    Add post upload and like apis

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CN, {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+  
+app.use(require('./routes'));
+
+server.listen(3333);
