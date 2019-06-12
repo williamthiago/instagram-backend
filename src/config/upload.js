3f4e7ee8822bb76ea671c4dea87618866faa48f1
William Thiago <williamthiago@gmail.com>
2019-06-12 01:41:45 -0400

    Add random names to images

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,11 +1,16 @@
 const multer = require('multer');
 const path = require('path');
+const uuid = require('uuid/v4');
 
 module.exports = {
     storage: new multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
         filename: (req, file, callback) => {
-            callback(null, file.originalname);
+            const { originalname } = file;
+            const ext = path.extname(originalname);
+
+            const name = `${uuid()}${ext}`;
+            callback(null, name);
         }
     })
 };
